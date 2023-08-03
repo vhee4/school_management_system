db4db1be03f6d41de6e98ec1d87f2a38e6d34604
gitpeters <petersab1111@gmail.com>
2023-08-03 17:31:03 +0100

    Removed commandline runner

--- src/main/java/com/SchoolManagement/School/Management/System/config/BeanConfig.java
+++ src/main/java/com/SchoolManagement/School/Management/System/config/BeanConfig.java
@@ -35,49 +35,49 @@ public class BeanConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public CommandLineRunner createDefaultUser(PlatformTransactionManager transactionManager) {
-        return args -> {
-            TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-
-            transactionTemplate.execute(status -> {
-
-
-                if (!staffRepository.findByEmail("admin@viteruni.org").isPresent()) {
-                    Optional<Roles> role = roleRepository.findByName("ROLE_ADMIN");
-                    Roles adminRole = null;
-                    if(!role.isPresent()){
-                        Roles newAdminUser= Roles.builder()
-                                .name("ROLE_ADMIN")
-                                .build();
-                        adminRole= roleRepository.save(newAdminUser);
-                    }else{
-                        adminRole = role.get();
-                    }
-
-                    Staff newUser = new Staff();
-                            newUser.setEmail("admin@viteruni.org");
-                    newUser.setFirstName("Vivian");
-                    newUser.setLastName("Chioma");
-                    newUser.setEnabled(true);
-                    newUser.setRoles(Collections.singleton(adminRole));
-                    newUser.setPassword(passwordEncoder().encode("admin"));
-
-//                    entityManager.persist(role); // Save the UserRole entity
-                    staffRepository.save(newUser); // Save the UserEntity entity
-                }
-                return null;
-            });
-            transactionTemplate.execute(status -> {
-
-                if (!departmentRepository.findByName("Science").isPresent()) {
-                    Department department = Department.builder()
-                            .name("Science")
-                            .build();
-                    departmentRepository.save(department); // Save the UserEntity entity
-                }
-                return null;
-            });
-        };
-    }
+//    @Bean
+//    public CommandLineRunner createDefaultUser(PlatformTransactionManager transactionManager) {
+//        return args -> {
+//            TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+//
+//            transactionTemplate.execute(status -> {
+//
+//
+//                if (!staffRepository.findByEmail("admin@viteruni.org").isPresent()) {
+//                    Optional<Roles> role = roleRepository.findByName("ROLE_ADMIN");
+//                    Roles adminRole = null;
+//                    if(!role.isPresent()){
+//                        Roles newAdminUser= Roles.builder()
+//                                .name("ROLE_ADMIN")
+//                                .build();
+//                        adminRole= roleRepository.save(newAdminUser);
+//                    }else{
+//                        adminRole = role.get();
+//                    }
+//
+//                    Staff newUser = new Staff();
+//                            newUser.setEmail("admin@viteruni.org");
+//                    newUser.setFirstName("Vivian");
+//                    newUser.setLastName("Chioma");
+//                    newUser.setEnabled(true);
+//                    newUser.setRoles(Collections.singleton(adminRole));
+//                    newUser.setPassword(passwordEncoder().encode("admin"));
+//
+////                    entityManager.persist(role); // Save the UserRole entity
+//                    staffRepository.save(newUser); // Save the UserEntity entity
+//                }
+//                return null;
+//            });
+//            transactionTemplate.execute(status -> {
+//
+//                if (!departmentRepository.findByName("Science").isPresent()) {
+//                    Department department = Department.builder()
+//                            .name("Science")
+//                            .build();
+//                    departmentRepository.save(department); // Save the UserEntity entity
+//                }
+//                return null;
+//            });
+//        };
+//    }
 }
